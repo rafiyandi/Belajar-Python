ce3a7839526054630f0c92ccba2a673a56e18e95
rafiyandi <rafiyandi1905@gmail.com>
2024-10-14 14:52:10 +0700

    mean

---
+++ library/example_np.py
@@ -0,0 +1,4 @@
+import numpy as np
+array_1 = np.array([2, 3, 6, 5])
+print(array_1)
+
---
+++ library/example_pandas.py
@@ -0,0 +1,9 @@
+import example_pandas as pd
+
+data = {
+    'Name': ['Lulu', 'Lala', 'Lili'],
+    'Age': [23, 45, 46]
+}
+
+df = pd.DataFrame(data)
+df
